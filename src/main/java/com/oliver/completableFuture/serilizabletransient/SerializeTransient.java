b20433fdafedcd65db886c6ad53fe081e6cf3413
oliver.wang@shein.com <286931986@qq.com>
2020-08-17 16:14:45 +0800

    transient/static attribute 与序列化

---
+++ src/main/java/com/oliver/completableFuture/serilizabletransient/SerializeTransient.java
@@ -0,0 +1,43 @@
+package com.oliver.completableFuture.serilizabletransient;
+
+import cn.hutool.json.JSONUtil;
+import lombok.Data;
+
+import java.io.*;
+
+/**
+ * @author Oliver Wang
+ * @description
+ * @created by IntelliJ IDEA 2018.3.3
+ * @date Create at 2020/8/17
+ * @since
+ */
+@Data
+public class SerializeTransient implements Serializable {
+    /**
+     * 静态属性 不会被序列化
+     */
+    private static int uid_id = 12525;
+    /**
+     * transient 修饰的属性不会被序列化
+     */
+    private transient String idCard;
+    private String name;
+
+    SerializeTransient(String idCard,String name){
+        this.idCard = idCard;
+        this.name = name;
+    }
+
+    public static void main(String[] args) {
+        SerializeTransient serializeTransient = new SerializeTransient("123456","zhansgan");
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("D:/test.txt"));
+            ObjectInputStream ios = new  ObjectInputStream(new FileInputStream("D:/test.txt"))){
+            oos.writeObject(serializeTransient);
+            serializeTransient = (SerializeTransient) ios.readObject();
+            System.out.println(serializeTransient);
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
