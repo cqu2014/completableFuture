ffee876caffaf72615523eed29365af220875436
oliver.wang@shein.com <286931986@qq.com>
2021-01-29 19:05:30 +0800

    多线程事务处理

--- src/main/java/com/oliver/completableFuture/CompletableFutureApplication.java
+++ src/main/java/com/oliver/completableFuture/CompletableFutureApplication.java
@@ -2,8 +2,10 @@ package com.oliver.completableFuture;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement
 public class CompletableFutureApplication {
 
 	public static void main(String[] args) {
